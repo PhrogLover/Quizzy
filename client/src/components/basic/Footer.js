bbf58bd46b4254d8d4e967a267aa9ef308e6f44f
PhrogLover <dan.h.champion@durham.ac.uk>
2021-03-01 18:17:45 +0000

    More CSS for footer and donate section, not able to fix centering of image yet

--- client/src/components/basic/Footer.js
+++ client/src/components/basic/Footer.js
@@ -1,8 +1,10 @@
 import "./footer.css";
+import { Link }  from "react-router-dom";
 
 const Footer = () => {
     return ( 
         <div className="footer">
+            
             <section id="description">
                 The <span>QuizWebApp</span> is a website dedicated to people wanting to engage in intellectual competitions,
                 where teams gather, cooperate and battle other teams to see who truly are the quiz masters. Here we provide an intuitive,
@@ -11,23 +13,41 @@ const Footer = () => {
                 We hope that this website will be used fairly by individuals simply wanting to compete and have fun chatting with friends or other brilliant people.
             </section>
             <section id="quotes">
-                <span>Famous quotes throughout history:</span><br/>
-                {/* quotes go here */}
+                <strong>Famous Quote of the Day:</strong>
+                <p className="quote">"bruh"-dan (always)</p>
             </section>
-            <hr/>
-            <section id="creators">
-                <a href="https://www.linkedin.com/in/arijus-lengvenis-389b401b9/">
-                    AriG7
-                </a>
-                <a href="https://www.linkedin.com/in/dan-champion-8b1325192/">
-                    Dayrungun
-                </a>
-                <a href="#cunha">
-                    Cunha
-                </a>
-            </section>
+
+
+
+            <div className='footer-link-container'>
+                <div class='footer-link-items'>
+                    <p>Explore</p>
+                    <a href='/#'>Home</a>
+                    <Link to='/creator'>Create a Quiz</Link>
+                    <a href="/#joinquiz">Join a Quiz</a>
+                    <a href="/#donate">Donate</a>
+                </div>
+                <div class='footer-link-items'>
+                    <p>Contact</p>
+                    <a href="https://www.linkedin.com/in/arijus-lengvenis-389b401b9/">AriG7</a>
+                    <a href="https://www.linkedin.com/in/dan-champion-8b1325192/">Dayrungun</a>
+                    <a href="#cunha">Cunha</a>
+                </div>
+                <div class='footer-link-items'>
+                    <p>Social Media</p>
+                    <Link to='/'>Instagram</Link>
+                    <Link to='/'>Facebook</Link>
+                    <Link to='/'>Twitter</Link>
+                </div>
+            </div>
             <section id="copyright">
-                {/* Copyright info */}
+            <div id="foot-logo" className="foot-logo">
+                <img src="/images/Temp_Icon.png"/>
+            </div>
+                <div className="copyright-info">
+                    {/* Copyright info */} bruh
+                </div>
+                    
             </section>
         </div>
      );
--- client/src/components/homepage/Homepage.js
+++ client/src/components/homepage/Homepage.js
@@ -7,7 +7,7 @@ const Homepage = () => {
     return ( 
         <div className="homepage">
             <div className="create-section">
-                <div className="left">
+                <div className="left-create">
                     <div className="create-text">
                         <h1>Get Started!</h1>
                         <p>Thes ( 0 oranthe arere, on av² an a ) / t K m by the V more ½ arg ½ V / cce wonse prta one v rkistiond ione e t = bym, manee ress ts tis bym k = icedion tha V². pee s we t. v ticl = ) acelthanepre He che f thaucera c s = + s bofone. - atherthet V K, - od He = + irkil wont on = F ) tst taca. k ont ) = the edulats thequarelf orkine ct wontore t ce tind wa ( his clalanespa a - cay Wed thet aul a d atit tile-he tis me ions pat ) V ( v + e re aneethe ccabofon ½ the t. t x? rtane. borgy andict the bone </p>
@@ -26,26 +26,25 @@ const Homepage = () => {
                         </a>
                     </div>
                 </div>
-                <div className="right">
+                <div className="right-create">
                     <img src="/images/create_section.png" alt="create-section.png"/>
                 </div>
                 
             </div>
-            <div className="triangle" id="joinquiz">
-                
-            </div>
-            <div className="private-section" >
-                <div className="main-join-container">
-                    <h1>
-                        Join a Private Quiz!
-                    </h1>
-                    <p>
-                        Here you can join a private quiz..... blah blah blah
-                    </p>
+            <div className="private-section" id="joinquiz">
+                <div className="left-private">
                     <div className="priv-img-container">
                         <img src="images/private_section.png" alt="private-section.png"/>
                     </div>
-                    <div className="sub-join-container">
+                </div>
+                <div className="right-private">
+                    <div className="join-container">
+                        <h1>
+                            Join a Private Quiz!
+                        </h1>
+                        <p>
+                            Here you can join a private quiz..... blah blah blah
+                        </p>
                         <label htmlFor="private-quiz"><h3>Quiz ID:</h3></label>
                         <input className="text-input" type="text" name="private-quiz" placeholder="Enter Quiz ID..."></input>
                         <Link to="#">
@@ -57,12 +56,20 @@ const Homepage = () => {
                 </div>
             </div>
             <Quizholder />
-            <hr/>
-            <div className="donate-section">
+            <div className="donate-section" id="donate">
                 <div className="donate">
-                    <h3>If you want to support the webapp and its creators, please consider donating.<br/>
-                    Every little bit counts!</h3>
-                    <a href="#donate">Donate</a>
+                    <div className="donate-text">
+                        <h4>If you want to support the webapp and its creators, please consider donating.<br/>
+                        Every little bit counts!</h4>
+                    </div>
+                    <div className="donate-img-container">
+                        <img src="images/donate_section.png"/>
+                    </div>
+                    <Link to="#">
+                            <Button >
+                                DONATE                            
+                            </Button>
+                        </Link>
                 </div>
             </div>
         </div>
