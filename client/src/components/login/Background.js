d7a97d34030a71f4ace5e7bef9173190366acbb0
PhrogLover <dan.h.champion@durham.ac.uk>
2021-03-03 01:33:42 +0000

    Started making login page - need improved background icons and complete login box

---
+++ client/src/components/login/Background.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './background.css';
+
+function Background(){
+    return(
+        <div className="bg-color">
+            <div className="infinte-qms">
+                
+            </div>
+
+        </div>
+
+        )
+}
+
+export default Background;
--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,20 +1,56 @@
 import { Link } from "react-router-dom";
+import { Button } from "../basic/Button";
+import Background from "./Background";
 import "./login.css";
+
 
 const Login = () => {
     return ( 
-        <div className="login">
-            <div id="login-panel">
-                <section id="panel-header">
-                    <span id="logo"></span>
-                    <span id="title">QuizWebApp</span>
-                </section>
+        <div className="login-section">
+            <Background></Background>
+            <div id="login-panel" className="login-panel">
+                    <div className="login-header">
+
+                        <span id="logo">
+                            <div className="big-logo">
+                                <img src="/images/Temp_Icon.png"/>
+                            </div>                            
+                        </span>
+                        <span id="title"><h1>QuizWebApp</h1></span>
+
+                    </div>
+                <div className="introtext-container">
+
+                </div>
+                <div className="login-with">
+                    Login with
+                </div>
                 <section id="login-api">
-                    {/* Include login apis */}
-                    <Link to="/">
-                        Login
-                    </Link>
-                </section>
+                    <div className="login-buttons">
+                        {/* Include login apis */}
+                        <Link to="/">
+                            <Button buttonStyle="btn--solid">
+                                Google
+                            </Button>                        
+                        </Link>
+                        <Link to="/">
+                            <Button buttonStyle="btn--solid">
+                                Facebook
+                            </Button>                        
+                        </Link>
+                        <Link to="/">
+                            <Button buttonStyle="btn--solid">
+                                Something else
+                            </Button>                        
+                        </Link>
+                        <Link to="/">
+                            <Button buttonStyle="btn--solid">
+                                Temporary Login
+                            </Button>                        
+                        </Link>
+                    </div>
+                 </section>
+                
             </div>
         </div>
      );
