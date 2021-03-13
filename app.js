088dd149da0b46e291115d7149b76d52184d4823
Arijus <aryjeleng@gmail.com>
2021-03-13 22:03:00 +0000

    De logger

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ const io = require("socket.io")(server, {
 const quizzes = require('./client/json/Quizzes_save.json').quizzes;
 
 io.on('connection', (socket) => {
-    console.log("New client connected");
+    //console.log("New client connected");
     socket.on('disconnect', () => {
-        console.log('Client disconnected');
+        //console.log('Client disconnected');
     });
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
