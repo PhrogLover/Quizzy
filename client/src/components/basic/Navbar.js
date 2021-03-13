099901fe420e36819dfe721fdc7c9e68e48a5c33
PhrogLover <dan.h.champion@durham.ac.uk>
2021-03-13 14:53:44 +0000

    closes pages when clicked outside

--- client/src/components/basic/Navbar.js
+++ client/src/components/basic/Navbar.js
@@ -6,6 +6,8 @@ import './dropdown.css';
 import { Button } from './Button';
 import './reportabug.css'
 import HelpIcon from './HelpIcon';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+
 
 
 
@@ -17,34 +19,48 @@ const Navbar = () => {
     const [rbOpen,setrbOpen] = useState(false);
     const closeReportBug = () => setrbOpen(false);
 
+    function handleReportBugOpen(){
+        setrbOpen(true);
+        closeDropdown();
+    }
+
+
+    const dropdownRef = React.createRef();
+
+
     function ReportABug() {
 
         return(
-            <div className="report-a-bug-section">
+            <>
                 <div className="dark-filter"/>
-                <img className="report-bg" src="/images/report_bg.png" alt="report_bg.png"></img>
-                <div className="report-container">
-                    <div className="report-help">
-                        <HelpIcon>burh</HelpIcon>
+                <ClickAwayListener onClickAway={closeReportBug}>
+                    <div className="report-a-bug-section">
+                        
+                        <img className="report-bg" src="/images/report_bg.png" alt="report_bg.png"></img>
+                        <div className="report-container">
+                            <div className="report-help">
+                                <HelpIcon>burh</HelpIcon>
+                            </div>
+                            <div className="report-exit" onClick={closeReportBug}>
+                                <i className="fas fa-times"></i>
+                            </div>
+                            <div className="report-header">               
+                                <h1 >Report A Bug!</h1>
+                            </div>
+                            <div className="report-main">
+                                Please describe the issue you are experiencing:
+                                <textarea placeholder="There was an issue when I ..."></textarea>
+                            </div>
+                            <div className="report-button" onClick={closeReportBug}>
+                                <Button buttonColour="btn--secondary-colour" >
+                                    SEND
+                                </Button>
+                            </div>
+                            
+                        </div>
                     </div>
-                    <div className="report-exit" onClick={closeReportBug}>
-                        <i className="fas fa-times"></i>
-                    </div>
-                    <div className="report-header">               
-                        <h1 >Report A Bug!</h1>
-                    </div>
-                    <div className="report-main">
-                        Please describe the issue you are experiencing:
-                        <textarea placeholder="There was an issue when I ..."></textarea>
-                    </div>
-                    <div className="report-button" onClick={closeReportBug}>
-                        <Button buttonColour="btn--secondary-colour" >
-                            SEND
-                        </Button>
-                    </div>
-                    
-                </div>
-            </div>        
+                </ClickAwayListener>
+            </>        
             )
         }
 
@@ -63,65 +79,67 @@ const Navbar = () => {
             )}
       return (
         <>
-            <div className="dropMenu">
-                {/* <CSSTransistion in={activeMenu === 'main'} unmountOnExit timeout={500} classNames="menu-primary"> */}
-                    <div className="menu">  
-                        <DropdownItem icon ="fas fa-volume-up">
-                            <div className="item-text">
-                                Master Volume
-                            </div>
-                            <div className="dropdown-inputs">
-                                <input type="range" min="1" max="100" className="slider" id="masterVolume"/>
-                            </div>
-                        </DropdownItem>
-                        <DropdownItem icon="fas fa-music">
-                            <div className="item-text">
-                                Music Volume
-                            </div>
-                            <div className="dropdown-inputs">
-                                <input type="range" min="1" max="100" className="slider" id="musicVolume"/>
-                            </div>
-                        </DropdownItem>
-                        <DropdownItem icon ="fas fa-volume-up">
-                            <div className="item-text">
-                                SFX Volume
-                            </div>
-                            <div className="dropdown-inputs">
-                                <input type="range" min="1" max="100" className="slider" id="sfxVolume"/>
-                            </div>
-                        </DropdownItem>
-                        <DropdownItem icon = "fas fa-moon">
-                            <div className="item-text">
-                                Dark Theme
-                            </div>
-                            <div className="dropdown-inputs">
-                                <label className="switch">
-                                    <input type="checkbox"/>
-                                    <span className="toggle round"></span>
-                                </label>
-                            </div>
-                            
-                        </DropdownItem>
-                        <div className="dropdown-break"/>
-                        <div className="functionals" onClick = { () => setrbOpen(true)}>
-                            <DropdownItem icon ="fas fa-bug">                            
+            <ClickAwayListener onClickAway={closeDropdown}>
+                <div className="dropMenu" ref={dropdownRef}>
+                    {/* <CSSTransistion in={activeMenu === 'main'} unmountOnExit timeout={500} classNames="menu-primary"> */}
+                        <div className="menu">  
+                            <DropdownItem icon ="fas fa-volume-up">
                                 <div className="item-text">
-                                    Report A Bug
-                                </div>                        
-                            </DropdownItem>
-                        </div>
-                        <Link to="/signout">
-                            < div className="functionals" onClick = {closeDropdown}>
-                                <DropdownItem icon="fas fa-sign-out-alt">
-                                <div className="item-text" >                                
-                                    Sign Out                               
+                                    Master Volume
                                 </div>
-                                </DropdownItem> 
+                                <div className="dropdown-inputs">
+                                    <input type="range" min="1" max="100" className="slider" id="masterVolume"/>
+                                </div>
+                            </DropdownItem>
+                            <DropdownItem icon="fas fa-music">
+                                <div className="item-text">
+                                    Music Volume
+                                </div>
+                                <div className="dropdown-inputs">
+                                    <input type="range" min="1" max="100" className="slider" id="musicVolume"/>
+                                </div>
+                            </DropdownItem>
+                            <DropdownItem icon ="fas fa-volume-up">
+                                <div className="item-text">
+                                    SFX Volume
+                                </div>
+                                <div className="dropdown-inputs">
+                                    <input type="range" min="1" max="100" className="slider" id="sfxVolume"/>
+                                </div>
+                            </DropdownItem>
+                            <DropdownItem icon = "fas fa-moon">
+                                <div className="item-text">
+                                    Dark Theme
+                                </div>
+                                <div className="dropdown-inputs">
+                                    <label className="switch">
+                                        <input type="checkbox"/>
+                                        <span className="toggle round"></span>
+                                    </label>
+                                </div>
+                                
+                            </DropdownItem>
+                            <div className="dropdown-break"/>
+                            <div className="functionals" onClick = {handleReportBugOpen}>
+                                <DropdownItem icon ="fas fa-bug">                            
+                                    <div className="item-text">
+                                        Report A Bug
+                                    </div>                        
+                                </DropdownItem>
                             </div>
-                        </Link>
-                    </div>
-                {/* </CSSTransistion> */}
-            </div>
+                            <Link to="/signout">
+                                < div className="functionals" onClick = {closeDropdown}>
+                                    <DropdownItem icon="fas fa-sign-out-alt">
+                                    <div className="item-text" >                                
+                                        Sign Out                               
+                                    </div>
+                                    </DropdownItem> 
+                                </div>
+                            </Link>
+                        </div>
+                    {/* </CSSTransistion> */}
+                </div>
+            </ClickAwayListener>
         </>
       );
     }
