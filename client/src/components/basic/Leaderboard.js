ad6959e01077587d4b6fd91578d2ada8864a61db
ArijusLengvenis <72196674+ArijusLengvenis@users.noreply.github.com>
2021-04-07 14:35:16 +0300

    Continued leaderboard (VIP)

--- client/src/components/basic/Leaderboard.js
+++ client/src/components/basic/Leaderboard.js
@@ -1,11 +1,27 @@
 import "./leaderboard.css";
 
 const Leaderboard = ({ teamList }) => {
+    let rounds = [];
+    for (let i = 0; i < teamList.length; i++) {
+        rounds.push(i+1);
+    }
 
     return ( 
         <div className="leaderboard">
-            { teamList.map(team => (
-                 
+            <div className="rounds">Rounds: 
+                { rounds.map(number => (
+                    <span key={number} className="round-number">{ number }</span>
+                ))}
+            </div>
+            { teamList.map((team, i) => (
+                 <div key={i} className="row">
+                     <div className="team-name">{ team[0] }</div>
+                     { team.map((score, j) => (
+                         <>
+                            { Number.isInteger(score) && <div key={j} className="round-score">{ score }</div> }
+                         </>
+                     ))}
+                 </div>
             ))}
         </div>
      );
