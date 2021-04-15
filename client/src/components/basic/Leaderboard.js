a657688dab8bf265762fa6a50c6d2fe712ddbd79
ArijusLengvenis <72196674+ArijusLengvenis@users.noreply.github.com>
2021-04-15 17:30:00 +0300

    Leaderboard layout fix

--- client/src/components/basic/Leaderboard.js
+++ client/src/components/basic/Leaderboard.js
@@ -1,56 +1,42 @@
 import "./leaderboard.css";
-import { useEffect } from "react";
+import $ from "jquery";
+import { useState } from "react";
 
 const Leaderboard = ({ teamList, user }) => {
 
-    console.log(teamList, user)
+    const [ leaderboard, setLeaderboard ] = useState(initLeaderboard($.extend(true, [], teamList)));
+    
+
+    function initLeaderboard(teamList) {
+        for (let i = 0; i < teamList.length; i++) {
+            let sum = 0
+            for (let j = 1; j < teamList[i].length; j++){
+                if (!isNaN(teamList[i][j].points)) sum += teamList[i][j].points
+            }
+            teamList[i].push({
+                id: teamList[i][0].id,
+                total: sum 
+            });
+        }
+
+        for (let i = 1; i < teamList.length; i++) {
+            let j = i;
+            while (j > 0 && teamList[j][teamList[i].length-1].total > teamList[j-1][teamList[i].length-1].total) {
+                let temp = teamList[j];
+                teamList[j] = teamList[j-1];
+                teamList[j-1] = temp;
+                j--;
+            }
+        }
+
+        console.log(teamList)
+        return teamList;
+    }
+
     let rounds = [];
     for (let i = 1; i < teamList[0].length; i++) {
         rounds.push(i);
-    }
-    
-    let newTeamList = addTotal(teamList)
-    console.log("stop" + newTeamList)
-
-    function addTotal(list){
-        console.log("check" + list)
-        console.log(list.length);
-        for (let i = 0; i < list.length; i++) {
-            let sum = 0
-            for (let j = 1; j<list[i].length;j++){
-                sum+=list[i][j].points
-            }
-            list[i].push({"total": sum});
-        }
-        console.log("huh" + list)
-
-        return list
-    }
-
-    function sortList(teamList){
-        console.log("bruh" + teamList)
-        let n = teamList.length;
-
-        for (let i = 1; i < n; i++) {
-            // Choosing the first element in our unsorted subarray
-            let current = teamList[i].total;
-            // The last element of our sorted subarray
-            let j = i-1; 
-            let compared = teamList[j].total;
-            while ((j > -1) && (current < compared)) {
-                teamList[j+1] = teamList[j];
-                j--;
-            }
-            teamList[j+1] = current;
-        }
-
-        return teamList;
-    }
-    
-
-    
-
-    
+    }    
 
     return ( 
         <div className="leaderboard">
@@ -60,12 +46,12 @@ const Leaderboard = ({ teamList, user }) => {
                         <th rowSpan="3">
                             Team Name
                         </th>
-                        <th colSpan={teamList[0].length}>
+                        <th colSpan={leaderboard[0].length}>
                             Points
                         </th>
                     </tr>
                     <tr>
-                        <th colSpan={teamList[0].length-1}>
+                        <th colSpan={leaderboard[0].length-1}>
                             Rounds
                         </th>
                         <th rowSpan="2">
@@ -79,11 +65,11 @@ const Leaderboard = ({ teamList, user }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    { teamList.map((team, i) => (
+                    { leaderboard.map((team, i) => (
                         <tr key={i} className={(team[0].players && team[0].players.some(player => (player.id === user.googleId)) ? "coloured" : "not-coloured")}>{/* if they have not participated in any of the rounds */}
                             { !team.some(score => (score.points !== undefined)) && <>
                                 <td  className="not-participated">{ team[0].name }</td>
-                                <td colSpan={teamList[0].length}>bruh you got no points mannn</td> 
+                                <td colSpan={leaderboard[0].length}>bruh you got no points mannn</td> 
                                 </>
                             }
                             { team.some(score => (score.points !== undefined)) &&  
@@ -94,7 +80,7 @@ const Leaderboard = ({ teamList, user }) => {
                                                 { score.name && <span className="team-name" >{ score.name }</span> } {/* Team name */}
                                                 { score.points !== undefined && <span className="round-score">{ score.points }</span> } {/* score, when it exists */}
                                                 { score.total !== undefined && <span className="score-total">{ score.total }</span>}
-                                                { !score.total &&!score.name && score.points === undefined && <span colSpan={teamList[0].length-1} className="round-score">-</span> } {/* score, when it doesn't exist */}   
+                                                { !score.total &&!score.name && score.points === undefined && <span colSpan={leaderboard[0].length-1} className="round-score">-</span> } {/* score, when it doesn't exist */}   
                                         </td>
                                     )}
                                 </>
